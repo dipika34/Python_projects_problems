b74e2939c228daa5d87cb98403b482333720cf15
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2026-06-03 16:40:26 +0530

    Create Practical6.py

---
+++ Practical6.py
@@ -0,0 +1,10 @@
+n = int(input("How many elements?"))
+list1 = []
+print("Enter the number of elements:")
+for i in range(0,n):
+    temp = int(input())
+    list1.append(temp)
+print("The elements are:")
+for temp in list1:
+    print(temp)
+   
